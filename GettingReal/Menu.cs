df28ad20cbb96ef5e1464ff9e60ada96704a1eef
Rasmus Vestergaard Jensen <rasmus797@hotmail.com>
2017-12-05 13:12:28 +0100

    fin kommentar med hvad jeg har ændret

--- GettingReal/Menu.cs
+++ GettingReal/Menu.cs
@@ -10,7 +10,23 @@ namespace GettingReal
     {
         public void ShowMenu()
         {
+            Console.WriteLine("Velkommen! Du har nu følgende valgmuligheder.");
+            Console.WriteLine("1. Søg efter medarbejder.");
+            Console.WriteLine("2. Opret Ny medarbejder.");
+            string menu_answer = Console.ReadLine();
 
+            switch(menu_answer)
+            {
+                case 1: Console.WriteLine("Søg efter medarbejder.");
+                    break;
+                case 2: Console.WriteLine("Opret medarbejder");
+                    break;
+                default: 
+                    Console.Clear();
+                    Console.WriteLine("Du skal indtaste et tal der passer til valgmulighederne");
+                    ShowMenu();
+                    break;
+            }
         }
         public void Show()
         {
