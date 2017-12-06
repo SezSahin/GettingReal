0782282039f7268ed7b8244f860ccaca84ed0efc
Mathias Vanner <mathias.vanner@live.dk>
2017-12-06 14:29:18 +0100

    Opret

--- GettingReal/Controller.cs
+++ GettingReal/Controller.cs
@@ -25,10 +25,34 @@ namespace GettingReal
                 SqlCommand cmd = new SqlCommand("spOpretBruger", con);
                 cmd.CommandType = System.Data.CommandType.StoredProcedure;
                 Console.WriteLine("Indtast Brugernavn \n");
-                cmd.Parameters.Add(new SqlParameter("@BrugerNavn", Console.ReadLine()));
+                cmd.Parameters.Add(new SqlParameter("@Brugernavn", Console.ReadLine()));
                 Console.WriteLine("Indtast Pasword \n");
                 cmd.Parameters.Add(new SqlParameter("@Password", Console.ReadLine()));
-                Console.Clear();
+                Console.WriteLine("Indtast Navn og mellem navn \n");
+                cmd.Parameters.Add(new SqlParameter("@Navn", Console.ReadLine()));
+                Console.WriteLine("Indtast Efternavn \n");
+                cmd.Parameters.Add(new SqlParameter("@Efternavn", Console.ReadLine()));
+                Console.WriteLine("Indtast Addresselinje 1 \n");
+                cmd.Parameters.Add(new SqlParameter("@Addresse", Console.ReadLine()));
+                Console.WriteLine("Indtast Addresselinje 2 (hvis der ikke er noget at skrive, skriv pik)\n");
+                cmd.Parameters.Add(new SqlParameter("@Addresse_l2", Console.ReadLine()));
+                Console.WriteLine("Indtast Addresse By\n");
+                cmd.Parameters.Add(new SqlParameter("@Addresse_by", Console.ReadLine()));
+                Console.WriteLine("Indtast Post nr.\n");
+                cmd.Parameters.Add(new SqlParameter("@Addresse_postnr", Console.ReadLine()));
+                Console.WriteLine("Indtast Telefon nr.\n");
+                cmd.Parameters.Add(new SqlParameter("@Tlf", Console.ReadLine()));
+                Console.WriteLine("Indtast CPR \n");
+                cmd.Parameters.Add(new SqlParameter("@CPR_nr", Console.ReadLine()));
+                Console.WriteLine("Indtast Konto nr. \n");
+                cmd.Parameters.Add(new SqlParameter("@konto_nr", Console.ReadLine()));
+                Console.WriteLine("Indtast Reg nr. \n");
+                cmd.Parameters.Add(new SqlParameter("@reg_nr", Console.ReadLine()));
+                Console.WriteLine("Indtast AfdelingsID \n");
+                cmd.Parameters.Add(new SqlParameter("@AfdelingsID", Console.ReadLine()));
+
+
+                    Console.Clear();
                 
                 cmd.ExecuteNonQuery();
                 }
